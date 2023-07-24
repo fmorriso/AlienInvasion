279066e38527192ce5a059385b98e92a25cb09ff
Frederick Morrison <frederick.morrison@outlook.com>
2023-07-24 13:44:02 -0600

    display python version when game starts

--- alien_invasion.py
+++ alien_invasion.py
@@ -234,8 +234,12 @@ class AlienInvasion:
 
         pygame.display.flip()
 
+@staticmethod
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 if __name__ == '__main__':
+    print(f'Python version {get_python_version()}')
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
